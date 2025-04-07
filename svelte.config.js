903134b2a8dd5dca1bd2434cec44ef1276f283b5
Melker Widén <melker.widen@gmail.com>
2025-04-08 01:37:44 +0200

    Change adapter, i dont know which attempt any more
    Removal of old needed

--- svelte.config.js
+++ svelte.config.js
@@ -1,9 +1,9 @@
-import adapter from "@sveltejs/adapter-auto";
+import adapter from "@sveltejs/adapter-node";
 import { vitePreprocess } from "@sveltejs/vite-plugin-svelte";
 
 export default {
   kit: {
-    adapter: adapter({ precompress: false }),
+        adapter: adapter({ precompress: false }),
   },
   preprocess: vitePreprocess(),
 };
