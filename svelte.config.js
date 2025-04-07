740f3707f18384ae7ec9e2bd68e8b637662003a5
Melker Widén <melker.widen@gmail.com>
2025-04-08 01:40:13 +0200

    Formatting

--- svelte.config.js
+++ svelte.config.js
@@ -3,7 +3,7 @@ import { vitePreprocess } from "@sveltejs/vite-plugin-svelte";
 
 export default {
   kit: {
-        adapter: adapter({ precompress: false }),
+    adapter: adapter({ precompress: false }),
   },
   preprocess: vitePreprocess(),
 };
