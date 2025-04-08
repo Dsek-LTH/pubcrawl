fc5bdb7e41e03fcb7b66883f546df87fd69a3994
Melker Widén <melker.widen@gmail.com>
2025-04-08 03:17:32 +0200

    Fix broken reset button and apply some code formatting

--- src/routes/(app)/counter/+page.svelte
+++ src/routes/(app)/counter/+page.svelte
@@ -24,36 +24,35 @@
 
 <div class="card bg-base-300">
     <div class="card-body">
-{#if theme}
-<div class="flex flex-row justify-between">
-    <h1 class="card-title">{theme.displayName} <span class="text-sm">(id: {data?.pubId})</span></h1>
+        {#if theme}
+        <div class="flex flex-row justify-between">
+            <h1 class="card-title">{theme.displayName} <span class="text-sm">(id: {data?.pubId})</span></h1>
 
-    <details class="dropdown dropdown-end">
-        <summary class="btn m-1">Danger zone <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" class="inline-block h-5 w-5 stroke-current"> <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M4 6h16M4 12h16M4 18h16"></path> </svg></summary>
-        <ul class="menu dropdown-content bg-base-100 rounded-box z-1 w-52 dropdown-right p-2 shadow-sm">
-            <button class="btn btn-warning w-full" formaction="?/reset">Reset</button>
-            <form method="POST" action="?/logout" use:enhance>
-                <button class="btn btn-primary w-full">Logout</button>
-            </form>
-        </ul>
-      </details>
+            <details class="dropdown dropdown-end">
+                <summary class="btn m-1">Danger zone <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" class="inline-block h-5 w-5 stroke-current"> <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M4 6h16M4 12h16M4 18h16"></path> </svg></summary>
+                <ul class="menu dropdown-content bg-base-100 rounded-box z-1 w-52 dropdown-right p-2 shadow-sm">
+                    <form method="POST" use:enhance>
+                        <button class="btn btn-warning w-full mb-1" formaction="?/reset">Reset</button>
+                        <button class="btn btn-primary w-full mt-1" formaction="?/logout">Logout</button>
+                    </form>
+                </ul>
+              </details>
+            </div>
+        {/if}
+
+        <br>
+
+        <form method="POST" use:enhance class="flex items-center sm:items-start flex-col sm:flex-row gap-4">
+            <div class="join join-vertical">
+            <button class="join-item text-5xl h-24 w-60 sm:w-24 btn btn-xl btn-success" formaction="?/increment">+</button>
+            <button class="join-item text-5xl h-24 w-60 sm:w-24 btn btn-xl btn-error" formaction="?/decrement">-</button>
+            </div>
+            {#if pub}
+            <PartialPubUpdateForm form={form} updateAction="?/updatePub" pub={pub}>
+            </PartialPubUpdateForm>
+        {/if}
+
+        </form>
+        <br>
     </div>
-{/if}
-
-
-<br>
-
-<form method="POST" use:enhance class="flex items-center sm:items-start flex-col sm:flex-row gap-4">
-    <div class="join join-vertical">
-    <button class="join-item text-5xl h-24 w-60 sm:w-24 btn btn-xl btn-success" formaction="?/increment">+</button>
-    <button class="join-item text-5xl h-24 w-60 sm:w-24 btn btn-xl btn-error" formaction="?/decrement">-</button>
-    </div>
-    {#if pub}
-    <PartialPubUpdateForm form={form} updateAction="?/updatePub" pub={pub}>
-    </PartialPubUpdateForm>
-{/if}
-
-</form>
-<br>
-</div>
 </div>
