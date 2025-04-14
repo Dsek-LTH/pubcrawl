4885ef1c44bb28fb4206d4e996a6f6278e6e943d
fgren <179166750+fgren@users.noreply.github.com>
2025-04-14 22:12:13 +0200

    Show logo based on theme

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,11 +1,26 @@
 <script lang="ts">
-import '../app.css';
+	import '../app.css';
 </script>
 
 <div class="m-0 flex min-h-screen w-screen flex-col p-0">
 	<nav class="navbar bg-base-300">
 		<div class="navbar-start">
-			<a href="/"><img src="/icons/favicon-512x512.png" alt="Pubcrawl logo" width="64" /></a>
+			<a href="/"
+				><img
+					class="dark:hidden"
+					src="/icons/favicon-512x512.png"
+					alt="Pubcrawl logo"
+					width="64"
+				/></a
+			>
+			<a href="/"
+				><img
+					class="hidden dark:block"
+					src="/icons/favicon-white-512x512.png"
+					alt="Pubcrawl logo"
+					width="64"
+				/></a
+			>
 			<a class="btn btn-ghost" href="/">Home</a>
 		</div>
 		<div class="navbar-middle">
@@ -17,8 +32,9 @@ import '../app.css';
 		</div>
 	</nav>
 
-    <main class="mx-auto w-11/12 sm:w-10/12 lg:w-8/12 mt-3 text-center sm:text-left flex flex-col flex-grow">
-    <slot />
-    </main>
+	<main
+		class="mx-auto mt-3 flex w-11/12 flex-grow flex-col text-center sm:w-10/12 sm:text-left lg:w-8/12"
+	>
+		<slot />
+	</main>
 </div>
-
