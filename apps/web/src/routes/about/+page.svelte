6c0246f31a89d8f9b2afa0c081aff48921f7ead8
fgren <179166750+fgren@users.noreply.github.com>
2025-05-08 01:06:06 +0200

    add logos to /about

--- apps/web/src/routes/about/+page.svelte
+++ apps/web/src/routes/about/+page.svelte
@@ -1,11 +1,20 @@
+<main class="flex flex-col items-center text-center mb-4">
+	<img class="w-6/12 sm:w-4/12 dark:hidden" src="/icons/favicon-512x512.png" alt="Pubcrawl logo" />
+	<img class="w-6/12 sm:w-4/12 hidden dark:block" src="/icons/favicon-white-512x512.png" alt="Pubcrawl logo" />
+
 <h1 class="text-2xl font-bold">Pubcrawl</h1>
 
 <p>
-	A web app that allows door attendants at TLTH pubs to register participants. The information is
-	updated in real-time, allowing others to see how many people are at each pub during the pub crawl.
+	Pubcrawl is a web app that allows door attendants at TLTH pubs to register participants. The information is
+	updated in real-time, allowing visitors to see how many people are at each pub during the pub crawl.
 </p>
 <br />
-<p>
+<p class="mt-10 mb-4">
 	Created by the <a class="link" href="https://dsek.se/committees/cpu">CPU</a> Committee, of the
 	<a class="link" href="https://dsek.se">D-guild</a>.
 </p>
+<a href="https://dsek.se/committees/cpu">
+<img class="w-5/12 sm:w-2/12 dark:hidden inline" src="https://raw.githubusercontent.com/Dsek-LTH/grafik/refs/heads/main/committee_logos/cpu/PNG/full/light.png" alt="Pubcrawl logo" />
+<img class="w-5/12 sm:w-2/12 hidden dark:inline" src="https://raw.githubusercontent.com/Dsek-LTH/grafik/refs/heads/main/committee_logos/cpu/PNG/full/dark.png" alt="Pubcrawl logo" />
+</a>
+</main>
