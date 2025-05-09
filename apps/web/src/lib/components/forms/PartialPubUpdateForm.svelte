66f5f8d0acceecd9d464dac868f781af2ad4888a
fgren <179166750+fgren@users.noreply.github.com>
2025-05-09 11:44:49 +0200

    warn if occupancy is too high

--- apps/web/src/lib/components/forms/PartialPubUpdateForm.svelte
+++ apps/web/src/lib/components/forms/PartialPubUpdateForm.svelte
@@ -24,14 +24,6 @@
 	<div class="stats min-w-60 bg-white shadow dark:bg-black">
 		<div class="stat text-center">
 			<span class="stat-title">Occupancy</span>
-			<span class="stat-value text-5xl font-bold">{pub.occupancy} / {pub.capacity}</span>
-			{#if form?.errors?.occupancy}
-				<p class="error">{form.errors.occupancy[0]}</p>
-			{/if}
+			<span class="stat-value text-5xl font-bold {pub.occupancy > pub.capacity ? "text-red-500" : ""}">{pub.occupancy} / {pub.capacity}</span>
 		</div>
-	</div>
-	{#if form?.errors?.general}
-		<p class="error">{form.errors.general[0]}</p>
-	{/if}
-	<!--<button class=" btn btn-sm" type="submit">Refetch</button>-->
 </form>
